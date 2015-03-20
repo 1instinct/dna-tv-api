187d45d898b8ade312623051175c0ecb767d6222
Aaron Smulktis <aaronsmulktis@gmail.com>
2015-03-20 18:01:32 -0400

    .

--- index.js
+++ index.js
@@ -1,12 +1,16 @@
 var express = require('express');
 var $ = require('jquery-latest');
 var app = express();
+var result = '{"FirstName":"John","LastName":"Doe","Email":"johndoe@johndoe.com","Phone":"123 dead drive"}';
+
 
 app.set('port', (process.env.PORT || 5000));
 app.use(express.static(__dirname + '/public'));
 
 app.get('/', function(request, response) {
-  response.send('Hello World!');
+	$.each($.parseJSON(result), function(k, v) {
+		alert(k + ' is ' + v);
+	});
 });
 
 app.listen(app.get('port'), function() {
