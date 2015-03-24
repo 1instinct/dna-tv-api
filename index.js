c390b600b7134ef22356567449283f1d78a3f831
Aaron Smulktis <aaronsmulktis@gmail.com>
2015-03-24 00:36:27 -0400

    .

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var app = express();
-var vids = "http://gdata.youtube.com/feeds/api/playlists/PLUfG5WpANuJpIm62ldjjpunTRb3hABEA4?v=2&alt=jsonc";
+var vids = "http://gdata.youtube.com/feeds/api/playlists/PLUfG5WpANuJpIm62ldjjpunTRb3hABEA4?v=2&alt=jsonc&max-results=50";
 var request = require('superagent');
 var port = 8080;
 
