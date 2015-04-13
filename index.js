d7e4b8a8d714ee58e4b36765a312a6dc802033a1
Aaron Smulktis <aaronsmulktis@gmail.com>
2015-04-13 14:41:27 -0400

    .

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ var port = 8080;
 app.use(function (req, res, next) {
 
     // Website you wish to allow to connect
-    res.setHeader('Access-Control-Allow-Origin', 'https://galoremag.com');
+    res.setHeader('Access-Control-Allow-Origin', 'http://galoremag.com');
 
     // Request methods you wish to allow
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
