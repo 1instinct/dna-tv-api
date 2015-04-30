d8205ac0ae6d44158e48f11e62832ddad6ebfcd3
Aaron Smulktis <aaronsmulktis@gmail.com>
2015-04-30 12:27:16 -0400

    update header origin allow to * (all)

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ var port = 8080;
 app.use(function (req, res, next) {
 
     // Website you wish to allow to connect
-    res.setHeader('Access-Control-Allow-Origin', 'http://galoremag.com');
+    res.setHeader('Access-Control-Allow-Origin', '*');
 
     // Request methods you wish to allow
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
