4ed7e0816a8797012c13318993804fa3049c8ac4
Aaron Smulktis <aaronsmulktis@gmail.com>
2015-06-28 13:57:35 -0400

    remove header set

--- index.js
+++ index.js
@@ -9,26 +9,6 @@ var vids = "https://www.googleapis.com/youtube/v3/playlists?part=id%2C+player&ch
 var request = require('superagent');
 var port = 8080;
 
-// Add headers
-app.use(function (req, res, next) {
-
-    // Website you wish to allow to connect
-    res.setHeader('Access-Control-Allow-Origin', '*');
-
-    // Request methods you wish to allow
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-
-    // Request headers you wish to allow
-    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-
-    // Set to true if you need the website to include cookies in the requests sent
-    // to the API (e.g. in case you use sessions)
-    res.setHeader('Access-Control-Allow-Credentials', true);
-
-    // Pass to next layer of middleware
-    next();
-});
-
 function process(arr) {
 	return arr.map(function(item) {
 		return {
