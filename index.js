6e0a32893a80f2b76a7efb6a260c03b5d3679f1c
Aaron Smulktis <aaronsmulktis@gmail.com>
2015-06-29 00:32:10 -0400

    .

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 var express = require('express');
 var app = express();
-// var Youtube = require("youtube-api");
+var google = require('googleapis');
+var urlshortener = google.urlshortener('v1');
+var params = { shortUrl: 'http://goo.gl/xKbRu3' };
+var API_KEY = 'AIzaSyD-_SyJzfqH4DrdwLsc3VXNeIH6jxqkufw';
+var Youtube = require("youtube-api");
 // GaloreTV 1
 // var vids = "https://gdata.youtube.com/feeds/api/playlists/PLPp3tIzLUEwaZfRUCuw1aJbDrTdgdm07b?v=2&alt=jsonc&max-results=49";
 // Galore TV 2
@@ -12,10 +16,19 @@ var vids = 'https://www.googleapis.com/youtube/v3/playlists?part=contentDetails%
 var request = require('superagent');
 var port = 80;
 
-// Youtube.authenticate({
-//     type: "key",
-// 	key: "AIzaSyD4GXoUQHtAEhNxKm40YkwOR1hPs1EKZhU"
-// });
+Youtube.authenticate({
+    type: "key",
+	key: "AIzaSyD4GXoUQHtAEhNxKm40YkwOR1hPs1EKZhU"
+});
+
+// get the long url of a shortened url
+urlshortener.url.get(params, function (err, response) {
+  if (err) {
+    console.log('Encountered error', err);
+  } else {
+    console.log('Long url is', response.longUrl);
+  }
+});
 
 function process(arr) {
 	return arr.map(function(item) {
