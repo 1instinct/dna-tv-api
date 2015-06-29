6d7bfa9d520f23f49ff5078e17eedd272dd617f5
Aaron Smulktis <aaronsmulktis@gmail.com>
2015-06-29 01:36:42 -0400

    .

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 var express = require('express');
 var app = express();
-// var google = require('googleapis');
-// var urlshortener = google.urlshortener('v1');
-// var params = { shortUrl: 'http://goo.gl/xKbRu3' };
-// // var Youtube = require("youtube-api");
-// var API_KEY = 'AIzaSyD-_SyJzfqH4DrdwLsc3VXNeIH6jxqkufw';
-// var channelId = 'UCyzzsgpNlmLBKYcXLM3Ro3g';
-// var plid = 'PLx0X0-cKhSOBXGK3Yr11j1HLa_ccrNO2G'
-// var maxResults = '33';
+var google = require('googleapis');
+var urlshortener = google.urlshortener('v1');
+var params = { shortUrl: 'http://goo.gl/xKbRu3' };
+// var Youtube = require("youtube-api");
+var API_KEY = 'AIzaSyD-_SyJzfqH4DrdwLsc3VXNeIH6jxqkufw';
+var channelId = 'UCyzzsgpNlmLBKYcXLM3Ro3g';
+var plid = 'PLx0X0-cKhSOBXGK3Yr11j1HLa_ccrNO2G'
+var maxResults = '33';
 
 // GaloreTV 1
 // var vids = "https://gdata.youtube.com/feeds/api/playlists/PLPp3tIzLUEwaZfRUCuw1aJbDrTdgdm07b?v=2&alt=jsonc&max-results=49";
@@ -20,23 +20,23 @@ var vids = 'https://www.googleapis.com/youtube/v3/playlists?part=contentDetails%
 var request = require('superagent');
 var port = 80;
 
-// Youtube.authenticate({
-//     type: "key",
-// 	key: "AIzaSyD4GXoUQHtAEhNxKm40YkwOR1hPs1EKZhU"
-// });
+Youtube.authenticate({
+    type: "key",
+	key: "AIzaSyD4GXoUQHtAEhNxKm40YkwOR1hPs1EKZhU"
+});
 
-// YouTube.Videos.List listVideosRequest = youtube.videos().list("snippet").setId(videoId);
-//           listVideosRequest.setKey(API_KEY);
-//           VideoListResponse listResponse = listVideosRequest.execute();
+YouTube.Videos.List listVideosRequest = youtube.videos().list("snippet").setId(videoId);
+          listVideosRequest.setKey(API_KEY);
+          VideoListResponse listResponse = listVideosRequest.execute();
 
 // get the long url of a shortened url
-// urlshortener.url.get(params, function (err, response) {
-//   if (err) {
-//     console.log('Encountered error', err);
-//   } else {
-//     console.log('Long url is', response.longUrl);
-//   }
-// });
+urlshortener.url.get(params, function (err, response) {
+  if (err) {
+    console.log('Encountered error', err);
+  } else {
+    console.log('Long url is', response.longUrl);
+  }
+});
 
 function process(arr) {
 	return arr.map(function(item) {
