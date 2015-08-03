9017be74446ce2da2d09f7fd4d9173736bbaeb5b
Aaron Smulktis <aaronsmulktis@gmail.com>
2015-08-03 11:53:10 -0400

    update request

--- index.js
+++ index.js
@@ -3,9 +3,9 @@ var app = express();
 
 var Youtube = require("youtube-api");
 
-var API_KEY = 'AIzaSyA0Ts8r7AdSbimwPQFKmbjQM8QKitGE95s';
-var channelId = 'UCyzzsgpNlmLBKYcXLM3Ro3g';
-var plid = 'PLx0X0-cKhSOBXGK3Yr11j1HLa_ccrNO2G'
+// var API_KEY = 'AIzaSyA0Ts8r7AdSbimwPQFKmbjQM8QKitGE95s';
+// var channelId = 'UCyzzsgpNlmLBKYcXLM3Ro3g';
+// var plid = 'PLx0X0-cKhSOBXGK3Yr11j1HLa_ccrNO2G';
 var maxResults = '33';
 
 // GaloreTV 1
@@ -15,14 +15,15 @@ var maxResults = '33';
 // GaloreTV 3
 // var vids = "https://www.googleapis.com/youtube/v3/playlists?part=contentDetails%2Cid%2Cplayer&channelId=UCyzzsgpNlmLBKYcXLM3Ro3g&maxResults=33&key=AIzaSyBLyj8zZcwPLTtQeTvA9j-fXCGZrMoz6TI";
 // var vids = 'https://www.googleapis.com/youtube/v3/playlists?part=contentDetails%2Cid%2Cplayer&channelId=UCyzzsgpNlmLBKYcXLM3Ro3g&maxResults=33&key=AIzaSyBVG4a32otoscjnNXbuw9lg1LwC2AvWss0';
+var vids = 'https://www.googleapis.com/youtube/v3/playlists?part=contentDetails&id=PLPp3tIzLUEwaZfRUCuw1aJbDrTdgdm07b&maxResults=33&fields=items(contentDetails%2Cetag%2Cid%2Ckind%2Cplayer%2Csnippet%2Cstatus)&key=AIzaSyA0Ts8r7AdSbimwPQFKmbjQM8QKitGE95s';
 
 var request = require('superagent');
 var port = 80;
 
-Youtube.authenticate({
-    type: "key",
-	key: "AIzaSyA0Ts8r7AdSbimwPQFKmbjQM8QKitGE95s"
-});
+// Youtube.authenticate({
+//     type: "key",
+// 	key: "AIzaSyA0Ts8r7AdSbimwPQFKmbjQM8QKitGE95s"
+// });
 
 // YouTube.Videos.List listVideosRequest = youtube.videos().list("snippet").setId(videoId);
 //           listVideosRequest.setKey(API_KEY);
@@ -38,16 +39,6 @@ function process(arr) {
 	});
 }
 
-// List your subcribers 
-Youtube.playlists.list({
-	"id": "PLPp3tIzLUEwaZfRUCuw1aJbDrTdgdm07b",
-    "part": "contentDetails",
-	// "mySubscribers": true,
-	"maxResults": 50
-	}, function (err, data) {
-    	console.log(err, data);
-});
-
 app.get('/vids', function (req, res) {
 	request.get(vids).end(function(err,response) {
 		if (err) {
@@ -59,6 +50,17 @@ app.get('/vids', function (req, res) {
 		}
 	});
 });
+
+// app.get('/vids', function (req, res) {
+// 	Youtube.playlists.list({
+// 		"id": "PLPp3tIzLUEwaZfRUCuw1aJbDrTdgdm07b",
+// 	    "part": "contentDetails",
+// 		// "mySubscribers": true,
+// 		"maxResults": 50
+// 		}, function (err, data) {
+// 	    	console.log(err, data);
+// 	});
+// });
 
 app.listen(port, function() {
 	console.log("Node app is running at localhost:" + port);
