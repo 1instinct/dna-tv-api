c4384e46df191bd5bce8c8d26bd50a85e0a6afbc
Aaron Smulktis <aaronsmulktis@gmail.com>
2015-08-22 21:00:41 -0400

    .

--- index.js
+++ index.js
@@ -1,33 +1,21 @@
 var express = require('express');
 var app = express();
 
-// var Youtube = require("youtube-api");
-
-// var API_KEY = 'AIzaSyA0Ts8r7AdSbimwPQFKmbjQM8QKitGE95s';
-// var channelId = 'UCyzzsgpNlmLBKYcXLM3Ro3g';
-// var plid = 'PLx0X0-cKhSOBXGK3Yr11j1HLa_ccrNO2G';
 var maxResults = '33';
 
 // GaloreTV 1
-// var vids = "https://gdata.youtube.com/feeds/api/playlists/PLPp3tIzLUEwaZfRUCuw1aJbDrTdgdm07b?v=2&alt=jsonc&max-results=49";
+// var vids = 'https://www.googleapis.com/youtube/v3/playlistItems?part=contentDetails%2C+snippet&maxResults=33&playlistId=PLPp3tIzLUEwaZfRUCuw1aJbDrTdgdm07b&fields=items(contentDetails%2Cetag%2Cid%2Csnippet%2Cstatus)&key=AIzaSyA0Ts8r7AdSbimwPQFKmbjQM8QKitGE95s';
 // Galore TV 2
-// var vids = "https://gdata.youtube.com/feeds/api/playlists/PLPp3tIzLUEwYySnVu0xLsKr13quhabBGr?v=2&alt=jsonc&max-results=49";
-// GaloreTV 3
-// var vids = "https://www.googleapis.com/youtube/v3/playlists?part=contentDetails%2Cid%2Cplayer&channelId=UCyzzsgpNlmLBKYcXLM3Ro3g&maxResults=33&key=AIzaSyBLyj8zZcwPLTtQeTvA9j-fXCGZrMoz6TI";
-// var vids = 'https://www.googleapis.com/youtube/v3/playlists?part=contentDetails%2Cid%2Cplayer&channelId=UCyzzsgpNlmLBKYcXLM3Ro3g&maxResults=33&key=AIzaSyBVG4a32otoscjnNXbuw9lg1LwC2AvWss0';
-var vids = 'https://www.googleapis.com/youtube/v3/playlistItems?part=contentDetails%2C+snippet&maxResults=33&playlistId=PLPp3tIzLUEwYySnVu0xLsKr13quhabBGr&fields=items(contentDetails%2Cetag%2Cid%2Csnippet%2Cstatus)&key=AIzaSyA0Ts8r7AdSbimwPQFKmbjQM8QKitGE95s';
+// var vids = 'https://www.googleapis.com/youtube/v3/playlistItems?part=contentDetails%2C+snippet&maxResults=33&playlistId=PLPp3tIzLUEwYySnVu0xLsKr13quhabBGr&fields=items(contentDetails%2Cetag%2Cid%2Csnippet%2Cstatus)&key=AIzaSyA0Ts8r7AdSbimwPQFKmbjQM8QKitGE95s';
+// Galore TV 3
+// var vids = 'https://www.googleapis.com/youtube/v3/playlistItems?part=contentDetails%2C+snippet&maxResults=33&playlistId=PLPp3tIzLUEwbkwSfDML6R12DCI5XwG6LH&fields=items(contentDetails%2Cetag%2Cid%2Csnippet%2Cstatus)&key=AIzaSyA0Ts8r7AdSbimwPQFKmbjQM8QKitGE95s';
+// Galore TV 4
+var vids = 'https://www.googleapis.com/youtube/v3/playlistItems?part=contentDetails%2C+snippet&maxResults=33&playlistId=PLPp3tIzLUEwaWgJtRGfkzkSot2ELUCR-l&fields=items(contentDetails%2Cetag%2Cid%2Csnippet%2Cstatus)&key=AIzaSyA0Ts8r7AdSbimwPQFKmbjQM8QKitGE95s';
+// Galore TV 5
+// var vids = 'https://www.googleapis.com/youtube/v3/playlistItems?part=contentDetails%2C+snippet&maxResults=33&playlistId=PLPp3tIzLUEwbukcmLprg-s4qOdw9mCEPD&fields=items(contentDetails%2Cetag%2Cid%2Csnippet%2Cstatus)&key=AIzaSyA0Ts8r7AdSbimwPQFKmbjQM8QKitGE95s';
 
 var request = require('superagent');
 var port = 80;
-
-// Youtube.authenticate({
-//     type: "key",
-// 	key: "AIzaSyA0Ts8r7AdSbimwPQFKmbjQM8QKitGE95s"
-// });
-
-// YouTube.Videos.List listVideosRequest = youtube.videos().list("snippet").setId(videoId);
-//           listVideosRequest.setKey(API_KEY);
-//           VideoListResponse listResponse = listVideosRequest.execute();
 
 function process(arr) {
 	return arr.map(function(items) {
@@ -56,17 +44,6 @@ app.get('/', function (req, res) {
 		}
 	});
 });
-
-// app.get('/vids', function (req, res) {
-// 	Youtube.playlists.list({
-// 		"id": "PLPp3tIzLUEwaZfRUCuw1aJbDrTdgdm07b",
-// 	    "part": "contentDetails",
-// 		// "mySubscribers": true,
-// 		"maxResults": 50
-// 		}, function (err, data) {
-// 	    	console.log(err, data);
-// 	});
-// });
 
 app.listen(port, function() {
 	console.log("Node app is running at localhost:" + port);
