6e973b59084bc484838705a65a90339414490bad
Aaron Smulktis <aaronsmulktis@gmail.com>
2015-11-18 13:58:05 -0500

    .

--- index.js
+++ index.js
@@ -7,14 +7,13 @@ var maxResults = '33';
 // var playlistId, nextPageToken, prevPageToken;
 
 // var apiKey = 'AIzaSyA0Ts8r7AdSbimwPQFKmbjQM8QKitGE95s';
-
 var apiKey = '1/g0-4IJCPvU-j66F0Sv6bJOfPpxdW2PSqT-iLgCeeEx4';
 
 var chanID = 'UCyzzsgpNlmLBKYcXLM3Ro3g';
 
 var allVidsID = 'UCyzzsgpNlmLBKYcXLM3Ro3g';
 
-var allVids = 'https://www.googleapis.com/youtube/v3/search?&channelId='+chanID+'&part=snippet&order=date&maxResults=20';
+var allVids = 'https://www.googleapis.com/youtube/v3/search?key='+apiKey+'&channelId='+chanID+'&part=snippet&order=date&maxResults=33'
 
 // GaloreTV 1
 // var vids = 'https://www.googleapis.com/youtube/v3/playlistItems?part=contentDetails%2C+snippet&maxResults=33&playlistId=PLPp3tIzLUEwaZfRUCuw1aJbDrTdgdm07b&fields=items(contentDetails%2Cetag%2Cid%2Csnippet%2Cstatus)&key=AIzaSyA0Ts8r7AdSbimwPQFKmbjQM8QKitGE95s';
