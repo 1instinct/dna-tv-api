5ac2c00e12f61c5c71ec1aaaa4bc5c68c28e6bcf
Aaron Smulktis <aaronsmulktis@gmail.com>
2015-11-18 13:23:56 -0500

    updated req

--- index.js
+++ index.js
@@ -4,11 +4,15 @@ var app = express();
 var maxResults = '33';
 
 // Define some variables used to remember state.
-var playlistId, nextPageToken, prevPageToken;
+// var playlistId, nextPageToken, prevPageToken;
+
+var apiKey = 'AIzaSyA0Ts8r7AdSbimwPQFKmbjQM8QKitGE95s';
+
+var chanID = 'UCyzzsgpNlmLBKYcXLM3Ro3g';
 
 var allVidsID = 'UCyzzsgpNlmLBKYcXLM3Ro3g';
 
-var allVids = 'https://www.googleapis.com/youtube/v3/playlistItems?part=contentDetails%2C+snippet&maxResults=33&playlistId=' + allVidsID + '-l&fields=items(contentDetails%2Cetag%2Cid%2Csnippet%2Cstatus)&key=AIzaSyA0Ts8r7AdSbimwPQFKmbjQM8QKitGE95s'
+var allVids = 'https://www.googleapis.com/youtube/v3/search?key='+apiKey+'&channelId='+chanID+'&part=contentDetails,snippet,id&order=date&maxResults=33'
 
 // GaloreTV 1
 // var vids = 'https://www.googleapis.com/youtube/v3/playlistItems?part=contentDetails%2C+snippet&maxResults=33&playlistId=PLPp3tIzLUEwaZfRUCuw1aJbDrTdgdm07b&fields=items(contentDetails%2Cetag%2Cid%2Csnippet%2Cstatus)&key=AIzaSyA0Ts8r7AdSbimwPQFKmbjQM8QKitGE95s';
@@ -38,17 +42,17 @@ function process(arr) {
 
 // Call the Data API to retrieve the playlist ID that uniquely identifies the
 // list of videos uploaded to the currently authenticated user's channel.
-function requestUserUploadsPlaylistId() {
-  // See https://developers.google.com/youtube/v3/docs/channels/list
-  var request = gapi.client.youtube.channels.list({
-    mine: true,
-    part: 'contentDetails'
-  });
-  request.execute(function(response) {
-    playlistId = response.result.items[0].contentDetails.relatedPlaylists.uploads;
-    requestVideoPlaylist(playlistId);
-  });
-}
+// function requestUserUploadsPlaylistId() {
+//   // See https://developers.google.com/youtube/v3/docs/channels/list
+//   var request = gapi.client.youtube.channels.list({
+//     mine: true,
+//     part: 'contentDetails'
+//   });
+//   request.execute(function(response) {
+//     playlistId = response.result.items[0].contentDetails.relatedPlaylists.uploads;
+//     requestVideoPlaylist(playlistId);
+//   });
+// }
 
 ///////
 
