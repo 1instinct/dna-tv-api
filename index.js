7395635045e8ccb8e7d6075d3ad6429023b958e7
Aaron Smulktis <aaronsmulktis@gmail.com>
2015-11-18 20:50:27 -0500

    revert, see if works

--- index.js
+++ index.js
@@ -3,19 +3,6 @@ var app = express();
 
 var maxResults = '33';
 
-// Define some variables used to remember state.
-// var playlistId, nextPageToken, prevPageToken;
-
-var apiKey = 'AIzaSyA0Ts8r7AdSbimwPQFKmbjQM8QKitGE95s';
-var accessToken = '1/Dt5cBhLxKG_EjyWZxYEJ7oADupiaJmrl_G_846BZ1mZIgOrJDtdun6zK6XiATCKT';
-
-
-var chanID = 'UCyzzsgpNlmLBKYcXLM3Ro3g';
-
-// var allVids = 'https://www.googleapis.com/youtube/v3/search?key='+apiKey+'&channelId='+chanID+'&part=snippet&order=date&maxResults=33';
-
-var allVids = 'https://www.googleapis.com/youtube/v3/search?key='+apiKey+'&channelId='+chanID+'&part=snippet&order=date&maxResults=33';
-
 // GaloreTV 1
 // var vids = 'https://www.googleapis.com/youtube/v3/playlistItems?part=contentDetails%2C+snippet&maxResults=33&playlistId=PLPp3tIzLUEwaZfRUCuw1aJbDrTdgdm07b&fields=items(contentDetails%2Cetag%2Cid%2Csnippet%2Cstatus)&key=AIzaSyA0Ts8r7AdSbimwPQFKmbjQM8QKitGE95s';
 // Galore TV 2
@@ -30,45 +17,15 @@ var vids = 'https://www.googleapis.com/youtube/v3/playlistItems?part=contentDeta
 var request = require('superagent');
 var port = 80;
 
-// Videos that are uploaded directly
-function processOriginalVids(arr) {
-	return map(function(items) {
-		return {
-			url: items.id.videoId,
-			_id: items.id.videoId,
-			title: items.snippet.title
-		}
-	});
-}
-
-// Videos that are saved and playlisted from other users
-function processSavedVids(arr) {
+function process(arr) {
 	return map(function(items) {
 		return {
 			url: items.snippet.resourceId.videoId,
-			_id: items.id.videoId,
+			_id: items.id,
 			title: items.snippet.title
 		}
 	});
 }
-
-///////
-
-// Call the Data API to retrieve the playlist ID that uniquely identifies the
-// list of videos uploaded to the currently authenticated user's channel.
-// function requestUserUploadsPlaylistId() {
-//   // See https://developers.google.com/youtube/v3/docs/channels/list
-//   var request = gapi.client.youtube.channels.list({
-//     mine: true,
-//     part: 'contentDetails'
-//   });
-//   request.execute(function(response) {
-//     playlistId = response.result.items[0].contentDetails.relatedPlaylists.uploads;
-//     requestVideoPlaylist(playlistId);
-//   });
-// }
-
-///////
 
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
@@ -77,13 +34,13 @@ app.use(function(req, res, next) {
 });
 
 app.get('/', function (req, res) {
-	request.get(allVids).end(function(err,response) {
+	request.get(vids).end(function(err,response) {
 		if (err) {
 			console.log(err);
 			res.status(404).send(err);
 		} else {
-			var allVids = processOriginalVids(response.body.items);
-			res.status(200).send(allVids);
+			var vids = process(response.body.items);
+			res.status(200).send(vids);
 		}
 	});
 });
