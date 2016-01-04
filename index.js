697af29bb5b5bd6e2529ec66c6116448ff052f2e
Aaron Smulktis <aaronsmulktis@gmail.com>
2016-01-04 12:01:04 -0500

    .

--- index.js
+++ index.js
@@ -2,7 +2,21 @@ var express = require('express');
 var app = express();
 
 var maxResults = '33',
-	playlist4 = 'PLPp3tIzLUEwaWgJtRGfkzkSot2ELUCR-l',
+	
+	listVintage1 = 'PLPp3tIzLUEwaZfRUCuw1aJbDrTdgdm07b',
+	listVintage2 = 'PLPp3tIzLUEwYySnVu0xLsKr13quhabBGr',
+	listVintage3 = 'PLPp3tIzLUEwbkwSfDML6R12DCI5XwG6LH',
+	listVintage4 = 'PLPp3tIzLUEwaWgJtRGfkzkSot2ELUCR-l',
+	listVintage5 = 'PLPp3tIzLUEwbukcmLprg-s4qOdw9mCEPD',
+
+	listLiveFrom = 'PLx0X0-cKhSOChWAkkEvSDv1nWfdIzXr9M',
+	listAskPush = 'PLx0X0-cKhSOAgUD3PjNTnEh4Unqgz1MxL',
+	listModel20 = 'PLx0X0-cKhSOAnlBpACK4BF2zR1MAZ4vLY',
+	listTeachMe = 'PLx0X0-cKhSOCCIGzz4vEy-KVhJ5JYuTFQ',
+	listGirls = 'PLx0X0-cKhSOCDflvS223SK4XELsHhuCCg',
+	listInBed = 'PLx0X0-cKhSOAr0sep7jVg4Yya7IU-Q-zv',
+	listBombshells = 'PLx0X0-cKhSOAmazTrHEZHSSc3RLQC6tQT',
+
 	accessToken = '1/Dt5cBhLxKG_EjyWZxYEJ7oADupiaJmrl_G_846BZ1mZIgOrJDtdun6zK6XiATCKT',
 	apiKey = 'AIzaSyA0Ts8r7AdSbimwPQFKmbjQM8QKitGE95s',
 	chanID = 'UCyzzsgpNlmLBKYcXLM3Ro3g',
@@ -16,26 +30,35 @@ var maxResults = '33',
 // Galore TV 3
 // var vids = 'https://www.googleapis.com/youtube/v3/playlistItems?part=contentDetails%2C+snippet&maxResults=33&playlistId=PLPp3tIzLUEwbkwSfDML6R12DCI5XwG6LH&fields=items(contentDetails%2Cetag%2Cid%2Csnippet%2Cstatus)&key=AIzaSyA0Ts8r7AdSbimwPQFKmbjQM8QKitGE95s';
 // Galore TV 4
-var vintage = 'https://www.googleapis.com/youtube/v3/playlistItems?part=contentDetails%2C+snippet&maxResults='+maxResults+'&playlistId='+playlist4+'&fields=items(contentDetails%2Cetag%2Cid%2Csnippet%2Cstatus)&key='+apiKey;
+var vintage = 'https://www.googleapis.com/youtube/v3/playlistItems?part=contentDetails%2C+snippet&maxResults='+maxResults+'&playlistId='+listVintage+'&fields=items(contentDetails%2Cetag%2Cid%2Csnippet%2Cstatus)&key='+apiKey;
 // Galore TV 5
 // var vids = 'https://www.googleapis.com/youtube/v3/playlistItems?part=contentDetails%2C+snippet&maxResults=33&playlistId=PLPp3tIzLUEwbukcmLprg-s4qOdw9mCEPD&fields=items(contentDetails%2Cetag%2Cid%2Csnippet%2Cstatus)&key=AIzaSyA0Ts8r7AdSbimwPQFKmbjQM8QKitGE95s';
+
+var liveFrom = 'https://www.googleapis.com/youtube/v3/playlistItems?part=contentDetails%2C+snippet&maxResults='+maxResults+'&playlistId='+listLiveFrom+'&fields=items(contentDetails%2Cetag%2Cid%2Csnippet%2Cstatus)&key='+apiKey,
+	askPush = 'https://www.googleapis.com/youtube/v3/playlistItems?part=contentDetails%2C+snippet&maxResults='+maxResults+'&playlistId='+listAskPush+'&fields=items(contentDetails%2Cetag%2Cid%2Csnippet%2Cstatus)&key='+apiKey,
+	model20 = 'https://www.googleapis.com/youtube/v3/playlistItems?part=contentDetails%2C+snippet&maxResults='+maxResults+'&playlistId='+listModel20+'&fields=items(contentDetails%2Cetag%2Cid%2Csnippet%2Cstatus)&key='+apiKey,
+	teachMe = 'https://www.googleapis.com/youtube/v3/playlistItems?part=contentDetails%2C+snippet&maxResults='+maxResults+'&playlistId='+listTeachMe+'&fields=items(contentDetails%2Cetag%2Cid%2Csnippet%2Cstatus)&key='+apiKey,
+	girls = 'https://www.googleapis.com/youtube/v3/playlistItems?part=contentDetails%2C+snippet&maxResults='+maxResults+'&playlistId='+listGirls+'&fields=items(contentDetails%2Cetag%2Cid%2Csnippet%2Cstatus)&key='+apiKey,
+	inBed = 'https://www.googleapis.com/youtube/v3/playlistItems?part=contentDetails%2C+snippet&maxResults='+maxResults+'&playlistId='+listInBed+'&fields=items(contentDetails%2Cetag%2Cid%2Csnippet%2Cstatus)&key='+apiKey,
+	bombshells = 'https://www.googleapis.com/youtube/v3/playlistItems?part=contentDetails%2C+snippet&maxResults='+maxResults+'&playlistId='+listBombshells+'&fields=items(contentDetails%2Cetag%2Cid%2Csnippet%2Cstatus)&key='+apiKey;
 
 var request = require('superagent');
 var port = 80;
 
-function process(arr) {
-	return arr.map(function(items) {
-		return {
-			url: items.id.videoId,
-			_id: items.id.videoId,
-			title: items.snippet.title,
-			desc: items.snippet.description,
-			thumbL: items.snippet.thumbnails.medium.url
-		}
-	});
-};
+// function process(arr) {
+// 	return arr.map(function(items) {
+// 		return {
+// 			url: items.id.videoId,
+// 			_id: items.id.videoId,
+// 			title: items.snippet.title,
+// 			desc: items.snippet.description,
+// 			// below not working for every request
+// 			thumbL: items.snippet.thumbnails.medium.url
+// 		}
+// 	});
+// };
 
-function processOld(arr) {
+function process(arr) {
 	return arr.map(function(items) {
 		return {
 			url: items.id.videoId,
@@ -71,8 +94,92 @@ app.get('/vintage', function (req, res) {
 			console.log(err);
 			res.status(404).send(err);
 		} else {
-			var vintage = processOld(response.body.items);
+			var vintage = process(response.body.items);
 			res.status(200).send(vintage);
+		}
+	});
+});
+
+app.get('/liveFrom', function (req, res) {
+	request.get(liveFrom).end(function(err,response) {
+		if (err) {
+			console.log(err);
+			res.status(404).send(err);
+		} else {
+			var liveFrom = process(response.body.items);
+			res.status(200).send(liveFrom);
+		}
+	});
+});
+
+app.get('/askPush', function (req, res) {
+	request.get(askPush).end(function(err,response) {
+		if (err) {
+			console.log(err);
+			res.status(404).send(err);
+		} else {
+			var askPush = process(response.body.items);
+			res.status(200).send(askPush);
+		}
+	});
+});
+
+app.get('/model20', function (req, res) {
+	request.get(model20).end(function(err,response) {
+		if (err) {
+			console.log(err);
+			res.status(404).send(err);
+		} else {
+			var model20 = process(response.body.items);
+			res.status(200).send(model20);
+		}
+	});
+});
+
+app.get('/teachMe', function (req, res) {
+	request.get(teachMe).end(function(err,response) {
+		if (err) {
+			console.log(err);
+			res.status(404).send(err);
+		} else {
+			var teachMe = process(response.body.items);
+			res.status(200).send(teachMe);
+		}
+	});
+});
+
+app.get('/girls', function (req, res) {
+	request.get(girls).end(function(err,response) {
+		if (err) {
+			console.log(err);
+			res.status(404).send(err);
+		} else {
+			var girls = process(response.body.items);
+			res.status(200).send(girls);
+		}
+	});
+});
+
+app.get('/inBed', function (req, res) {
+	request.get(inBed).end(function(err,response) {
+		if (err) {
+			console.log(err);
+			res.status(404).send(err);
+		} else {
+			var inBed = process(response.body.items);
+			res.status(200).send(inBed);
+		}
+	});
+});
+
+app.get('/bombshells', function (req, res) {
+	request.get(bombshells).end(function(err,response) {
+		if (err) {
+			console.log(err);
+			res.status(404).send(err);
+		} else {
+			var bombshells = process(response.body.items);
+			res.status(200).send(bombshells);
 		}
 	});
 });
