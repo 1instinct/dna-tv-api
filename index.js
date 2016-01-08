aec4815620ee20b5e92337b5fc38d830010f9b69
Aaron Smulktis <aaronsmulktis@gmail.com>
2016-01-08 16:29:56 -0500

    CSV?

--- index.js
+++ index.js
@@ -72,7 +72,7 @@ function ConvertToCSV(objArray) {
     for (var i = 0; i < array.length; i++) {
         var line = '';
         for (var index in array[i]) {
-            if (line != '') line += ','
+            if (line != '') {line += ','};
 
             line += array[i][index];
         }
