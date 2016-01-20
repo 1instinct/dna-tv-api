5c161570304df0b7eec8875cb32b40d39d7930e2
Aaron Smulktis <aaronsmulktis@gmail.com>
2016-01-20 15:40:14 -0500

    add ternary conditional for maxres

--- index.js
+++ index.js
@@ -131,7 +131,7 @@ function processList(arr) {
 	return arr.map(function(items) {
 	
 		function hiRes(img) {
-			if (!("maxres" in img) === true) {
+			if (!("items.snippet.thumbnails.maxres.url" in img) === true) {
 				return items.snippet.thumbnails.high.url;
 			} else {
 				return items.snippet.thumbnails.maxres.url;
