4c16d00cf29321c0eb87b808b8badf418c389d09
Aaron Smulktis <aaronsmulktis@gmail.com>
2016-03-29 11:58:14 -0400

    .

--- index.js
+++ index.js
@@ -247,8 +247,6 @@ function processOther(arr) {
 	});
 };
 
-debugger;
-
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
